0dc34f7aef6cbcfaefaa8f71a843b7d384c530d7
z-zaremska <zuza.zaremska@gmail.com>
2023-03-05 09:36:08 +0100

    Recipe tests, serializer,view.

--- app/core/models.py
+++ app/core/models.py
@@ -54,7 +54,7 @@ class Recipe(models.Model):
     description = models.TextField(blank=True)
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    tink = models.CharField(max_length=255, blank=True)
+    link = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.title

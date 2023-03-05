4b547660fee61c925c3611d1817274f9cd193c6c
z-zaremska <zuza.zaremska@gmail.com>
2023-03-05 19:00:06 +0100

    Added blank line.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,6 +6,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from .. import models
 
+
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return a new user."""
     return get_user_model().objects.create_user(email, password)
